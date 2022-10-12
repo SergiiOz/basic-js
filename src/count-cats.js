a55115210409776c539492d73e548400969dcee2
Sergii Ozinkovskyi <s.ozinkovskyi@gmail.com>
2022-10-12 22:08:31 +0300

    feat: add task count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -3,7 +3,7 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
  * @example
@@ -14,11 +14,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrixArr) {
+    //check
+    if (!Array.isArray(matrixArr) || !matrixArr) {
+        let error = new Error("Wrong: data isn't array");
+        return console.log(error);
+    }
+
+    let sumCats = 0;
+    matrixArr.forEach((nestedArr) => {
+        for (let i = 0; i < nestedArr.length; i++) {
+            if (nestedArr[i] === '^^') {
+                sumCats++;
+            }
+        }
+    });
+    return sumCats;
 }
 
 module.exports = {
-  countCats
+    countCats,
 };
